017ddbeda0ae0ab932390c967f9bdcb9e40fc62f
algorni <alberto.gorni@microsoft.com>
2019-05-10 15:45:01 +0000

    initial commit

---
+++ source/GroupEnrollmentHelper/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+
+namespace GroupEnrollmentHelper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("------------------------------------");
+            Console.WriteLine("  Hello from GroupEnrollmentHelper");
+            Console.WriteLine("------------------------------------");
+            Console.WriteLine();
+
+            if (args.Count() == 2)
+            {
+                var masterKey = args.First();
+                var registrationId = args.Last();
+                
+                byte[] masterKeyBytes = Convert.FromBase64String(masterKey);
+
+                Console.WriteLine("Computing derived Key...");
+
+                string derivedKey = Utils.ComputeDerivedSymmetricKey(masterKeyBytes, registrationId);
+
+                Console.WriteLine("Derived key calcualted...");
+                Console.WriteLine();
+                Console.WriteLine(derivedKey);
+            }
+            else
+            {
+                Console.WriteLine("You need to provide your masterKey and registratioId as command parameters.");
+                Console.WriteLine("GroupEnrollmentHelper <masterKey> <registrationID>");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
---
+++ source/GroupEnrollmentHelper/Utils.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+public static class Utils
+{
+    public static string ComputeDerivedSymmetricKey(byte[] masterKey, string registrationId)
+    {
+        using (var hmac = new HMACSHA256(masterKey))
+        {
+            return Convert.ToBase64String(hmac.ComputeHash(Encoding.UTF8.GetBytes(registrationId)));
+        }
+    }
+}
